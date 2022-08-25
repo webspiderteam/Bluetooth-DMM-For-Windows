b04c07199248227d6e8f07aae32fb13d4ddcd79c
WebSpiderTeam <webspiderteam@hotmail.com>
2022-08-25 19:57:18 +0300

    CrashLog ability

--- BluetoothDMM WPF/BluetoothDMM/App.xaml.cs
+++ BluetoothDMM WPF/BluetoothDMM/App.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace BluetoothDMM
 {
@@ -15,6 +18,7 @@ namespace BluetoothDMM
 
         protected override void OnStartup(StartupEventArgs e)
         {
+            Application.Current.DispatcherUnhandledException += new DispatcherUnhandledExceptionEventHandler(AppDispatcherUnhandledException);
             // Step 1 - Load the splash screen
             SplashScreen splash = new SplashScreen("Assets/SplashScreen.scale-100.png");
             splash.Show(false, true);
@@ -35,6 +39,37 @@ namespace BluetoothDMM
             // Step 5 - show the page
             splash.Close(TimeSpan.FromMilliseconds(SPLASH_FADE_TIME));
             main.Show();
+        }
+
+        void AppDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+#if DEBUG// In debug mode do not custom-handle the exception, let Visual Studio handle it
+            e.Handled = false;
+#else
+            ShowUnhandledException(e);
+#endif
+
+        }
+
+        void ShowUnhandledException(DispatcherUnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+
+            string errorMessage = string.Format("An application error occurred.\n" +
+                "Please check whether your data is correct and repeat the action. " +
+                "If this error occurs again there seems to be a more serious malfunction in the application, and you better close it.\n\n" +
+                "Error: {0}\n\nDo you want to continue?\n" +
+                "(if you click Yes you will continue with your work, if you click No the application will close)",
+            e.Exception.Message + (e.Exception.InnerException != null ? "\n" +
+            e.Exception.InnerException.Message : null));
+            string time= DateTime.Now.ToString("dd-MM-yyyy_HH_mm_ss")
+            File.AppendAllText("crashlog_" + time + ".txt", "error catch Error: " + e.ToString() + System.Environment.NewLine);
+            File.AppendAllText("crashlog_" + time + ".txt", errorMessage + System.Environment.NewLine);
+            if (MessageBox.Show(errorMessage, "Application Error", MessageBoxButton.YesNoCancel, MessageBoxImage.Error) == MessageBoxResult.No)
+                if (MessageBox.Show("WARNING: The application will close. Any changes will not be saved!\nDo you really want to close it?", "Close the application!", MessageBoxButton.YesNoCancel, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+                {
+                    Application.Current.Shutdown();
+                }
         }
         public ResourceDictionary ThemeDictionary
         {
