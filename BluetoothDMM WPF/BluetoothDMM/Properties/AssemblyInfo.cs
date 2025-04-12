cf07193b4da05054b117761495e11fdc7724481d
WebSpiderTeam <webspiderteam@hotmail.com>
2025-04-12 22:31:31 +0300

    - Bump version 1.27 and Release

--- BluetoothDMM WPF/BluetoothDMM/Properties/AssemblyInfo.cs
+++ BluetoothDMM WPF/BluetoothDMM/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("WebSpiderTeam")]
 [assembly: AssemblyProduct("Bluetooth DMM For Windows")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.26.0.0")]
-[assembly: AssemblyFileVersion("1.26.0.0")]
+[assembly: AssemblyVersion("1.27.0.0")]
+[assembly: AssemblyFileVersion("1.27.0.0")]
